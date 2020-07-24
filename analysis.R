74b62efcaebe13a7c79292c1f6c5cbfe2eb680b5
Nimaye Garodia <garodia@Nimayes-Mac.local>
2020-07-24 17:27:45 +0530

    Add readr

--- analysis.R
+++ analysis.R
@@ -10,7 +10,9 @@ library(ggpubr)
 library(plotrix)
 library(readr)
 
-reviews_data <- reviews_Musical_Instruments_5 
+reviews_data <- read_csv("reviews_Musical_Instruments_5.csv")
 
 reviews_data$`review rating` <- reviews_data$`helpful/0` / reviews_data$`helpful/1`
 reviews_data_proper <- reviews_data[reviews_data$`review rating` != "NaN",]
+
+View(reviews_data_proper)
